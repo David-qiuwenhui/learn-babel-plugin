c9b4fb217e701a6755602adbaabc65ebe53bc15d
qiuwenhui <1059713136@qq.com>
2024-09-19 22:56:49 +0800

    🎉 init: init

---
+++ example/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button @click="handleClick">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const handleClick = () => {
+      // 1. 在 dev 环境下执行
+      // 2. 在 prod 环境下不执行
+      if (DEBUG) {
+        console.log("click");
+        const a = 10;
+        const b = 10;
+        console.log(a + b);
+      }
+    };
+
+    return { handleClick };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
